799fc84baf0f94275ab7c5da9c0ddb27abc8ecd5
charly <cdsaenz@gmail.com>
2023-01-11 23:34:03 -0300

    Mejoras visuales navbar etc

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,28 @@ for (i; i < menuLinks.length; i++) {
   link.addEventListener("click",function(e){
         // adonde apunta el menu?
         let target_id  = e.target.dataset.id; 
-        // fijar como activo
-        
+        // anterior activo, desactivar y activar este.
+        setMenuActive(e.target);        
         // primero, cambiar el color de todo a default
         changeClassElementsColor('section-title','darkslategray');        
         // colocar un color que destaque
-        document.getElementById(target_id).style.color = "#6CB4EE";
+        document.getElementById(target_id).style.color = "mediumpurple";
   });
+}
+
+/**
+ * Cambiar menu activo
+ */
+function setMenuActive(newActiveMenuItem) {
+  
+  // quitar todos los activos (deberia ser uno)
+  var elements = document.getElementsByClassName('menu-link active');
+  for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.remove('active');
+  }
+
+  // fijar como activo el elemento pasado
+  newActiveMenuItem.classList.add('active');
 }
 
 /**
