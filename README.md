d29e7c43abbf7c558977434b48157a9b84881a80
charly <cdsaenz@gmail.com>
2023-01-12 14:18:21 -0300

    Fetch profile data and localstorage. Projects accordion

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # Programar 4.0 CV
 
-# Datos de acceso
+# Acceso a página pública
 - URL : https://cdsaenz.github.io/cv/
+
+# Caracteristicas aplicadas
+- Bootstrap 5
+- FontAwesome
+- Google Font(s)
+- Hoja de estilos propia
+    - Media Queries
+- Javascript propio
+    - Javascript puro
+    - Modificacion DOM via Javascript
+    - Uso de localStorage
+    - Uso de fetch
+    - Uso de eventos (addEventListener)
+    - Uso de iterador for
+    - Uso de funciones
+- HTML
+    - Elementos semánticos
+    - Nav de menu superior (navbar)
+    - Listas desordenadas
+    - Diseño Responsive
--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,12 @@
  * Scripts del proyecto
  */
 
+/* Obtener de storage o remoto */
+getProfileData();
 
+/**
+ * Cosmetica cuando se clickea un menu item
+ */
 let i = 0;
 const menuLinks = document.getElementsByClassName('menu-link');
 for (i; i < menuLinks.length; i++) {
@@ -17,6 +22,97 @@ for (i; i < menuLinks.length; i++) {
         // colocar un color que destaque
         document.getElementById(target_id).style.color = "mediumpurple";
   });
+}
+
+/**
+ * Evento para ocultar/mostrar proyectos
+ */
+const projectsToggle = document.getElementById('projects-toggle');
+projectsToggle.addEventListener("click",function(e){
+  toggleElementVisibility("projects-list");
+});
+
+
+/**
+ * Funciones
+ */
+
+/**
+ * Obtener data de localstore o random para el perfil
+ * 500 daily results max plan free ?
+ */
+
+function getProfileData() {
+
+  let storage  = localStorage.getItem("profile-data");
+  
+  /* no hallado, traer remoto */
+  if (storage === null) {
+    fetch('https://randomuser.me/api/')
+      .then((response) => response.json())
+      .then((data) => updateProfileDataFromRemote(data));
+  }
+  else {
+    let profile  = JSON.parse(storage);    
+    showProfileData(profile);
+  }
+
+}
+
+
+
+
+/**
+ * Procesar data random remota
+ */
+function updateProfileDataFromRemote(data) {
+
+    const result   = data.results[0];
+    const location = result.location;
+
+    // obtener data
+    const profile    = {};
+    profile.name     = result.name.first + ' ' + result.name.last;
+    profile.email    = result.email;
+    profile.phone    = result.phone;
+    profile.age      = result.dob.age;
+    profile.picsrc   = result.picture.large;
+    profile.location = location.street.name + ' ' + location.street.number + ' ' +
+                       location.postcode + ' ' + location.city + ' ' + 
+                       location.state + ' ' + location.city + ' ' + location.country;
+
+    // store in storage.
+    localStorage.setItem("profile-data", JSON.stringify(profile));
+    // show in DOM
+    showProfileData(profile);
+}
+
+/**
+ * Profile data a DOM
+ */
+function showProfileData(profile) {
+
+  // inyectar en el DOM
+  document.getElementById('profile-name').innerHTML = profile.name;
+  document.getElementById('profile-email').innerHTML = profile.email;
+  document.getElementById('profile-phone').innerHTML = profile.phone;
+  document.getElementById('profile-age').innerHTML = profile.age;
+  document.getElementById('profile-location').innerHTML = profile.location;
+  document.getElementById('profile-picture').src = profile.picsrc;  
+}
+
+/**
+ * Toggle ver o ocultar element
+ */
+function toggleElementVisibility(elementId) {
+  const element    = document.getElementById(elementId);
+  let displayStyle = element.style.display;
+  if (displayStyle == 'block') {
+    element.style.display = 'none';
+  }
+  else {
+    element.style.display = 'block';
+  }
 }
 
 /**
@@ -43,4 +139,5 @@ function changeClassElementsColor(className, color) {
         elements[i].style.color = color;
     }
 }
+
 
